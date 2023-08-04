d8add5709cad24981f298c7446ef68bdf82272d6
mosquida <mosquidacarljustine@gmail.com>
2023-08-04 15:37:17 +0800

    Added Account POST:test_new_order and GET:query_order

--- coinspro_py/coins_account.py
+++ coinspro_py/coins_account.py
@@ -22,7 +22,6 @@ class CoinsAccount:
     
     def _send_request(self, method, endpoint, params=None):
         url = self.base_url + endpoint
-        print(url)
         headers = {
             "X-COINS-APIKEY": self.api_key
         }
@@ -78,8 +77,55 @@ class CoinsAccount:
             params["newOrderRespType"] = new_order_resp_type
         if stp_flag:
             params["stpFlag"] = stp_flag
-        if recv_window:
-            params["recvWindow"] = recv_window
+        
+        params["signature"] =self._create_signature(params)
+        return self._send_request("POST", endpoint="/openapi/v1/order", params=params)
+
+    def test_new_order(self, symbol, side, orderType, time_in_force='GTC', quantity=None, quote_order_qty=None,
+                    price=None, new_client_order_id=None, stop_price=None, new_order_resp_type=None,
+                    stp_flag=None, recv_window=5000, quoteOrderQty=None):
+        # Required params
+        params = {
+            'symbol': symbol,
+            'side': side,
+            'type': orderType,
+            'recvWindow': recv_window,
+            'timestamp': int(time.time() * 1000),
+        }
+        
+        if time_in_force:
+            params["timeInForce"] = time_in_force
+        if quoteOrderQty:
+            params["quoteOrderQty"] = quoteOrderQty
+        if quantity:
+            params["quantity"] = quantity
+        if quote_order_qty:
+            params["quoteOrderQty"] = quote_order_qty
+        if price:
+            params["price"] = price
+        if new_client_order_id:
+            params["newClientOrderId"] = new_client_order_id
+        if stop_price:
+            params["stopPrice"] = stop_price
+        if new_order_resp_type:
+            params["newOrderRespType"] = new_order_resp_type
+        if stp_flag:
+            params["stpFlag"] = stp_flag
         
         params["signature"] =self._create_signature(params)
         return self._send_request("POST", endpoint="/openapi/v1/order/test", params=params)
+
+    def query_order(self, orderId=None, origClientOrderId=None, recv_window=5000):
+        # Required params
+        params = {
+            'recvWindow': recv_window,
+            'timestamp': int(time.time() * 1000),
+        }
+        
+        if orderId:
+            params["orderId"] = orderId
+        if origClientOrderId:
+            params["origClientOrderId"] = origClientOrderId
+        
+        params["signature"] =self._create_signature(params)
+        return self._send_request("GET", endpoint="/openapi/v1/order", params=params)
